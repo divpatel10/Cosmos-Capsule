259f47dc1c159ffb9b834f3a689ba475af8c5144
div <div.pat499@gmail.com>
2022-02-27 18:32:05 -0500

    added debug tool

--- factsheetapi/views.py
+++ factsheetapi/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# request handler
+
+# create a view 
+def say_hello(req):
+    return render(req, 'debug.html')
