d8863427e6cbee5c47fbc38252687b47870ff09c
div <div.pat499@gmail.com>
2022-04-02 22:17:33 -0400

    fixed mass query internal server error bug

--- factsheet.py
+++ factsheet.py
@@ -22,15 +22,18 @@ def home():
 @app.get("/factsheet", status_code=200)
 async def factsheet(req: Request, params:Planet = Depends()):
     cur_data = scraped_data
+    print("All Query Params",req.query_params)
+    print("Planet Model Query Params",params )
 
     if req.query_params is not None:
         if req.query_params.get("units") is not None and req.query_params.get("units")  == IMPERIAL_DATA_TYPE:
             cur_data = get_factsheet(KEY_AS_PLANET,IMPERIAL_DATA_TYPE)
         
         if params.mass is not None :
-            cur_data = scraped_data[scraped_data["Mass"] == params.mass]
-
-
+            col_name = [col for col in scraped_data.columns if 'Mass' in col][0]
+            cur_data = scraped_data[scraped_data[col_name] == str(params.mass)]
+            print("Foud Matching Column Name: ",col_name)
+    
     data = cur_data.to_json(orient='index')
     return json.loads(data)
 
