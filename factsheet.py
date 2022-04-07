feada384cdc33e142819f52fc32e7e8f31af5329
div <div.pat499@gmail.com>
2022-04-07 15:38:58 -0400

    fix bug - units query not working

--- factsheet.py
+++ factsheet.py
@@ -23,6 +23,7 @@ def home():
 @app.get("/factsheet", status_code=200)
 
 async def factsheet(req: Request, params:Planet = Depends()):
+    global cur_data
     cur_data = scraped_data
 
     print("All Query Params", (dict(req.query_params)))
@@ -32,7 +33,9 @@ async def factsheet(req: Request, params:Planet = Depends()):
        
         if req.query_params.get("units") is not None \
              and req.query_params.get("units")  == IMPERIAL_DATA_TYPE:
+            print('-'*40)
             cur_data = scraped_data_us
+        print(cur_data)
 
         if req.query_params.get("planet") is not None:
             data = json.loads(cur_data.to_json(orient='index'))
@@ -44,14 +47,12 @@ async def factsheet(req: Request, params:Planet = Depends()):
         
         else:
             planet_properties = [v[0] for v in params]
-            cur_data = scraped_data.copy(deep=True)
             for property in planet_properties:
 
                 if req.query_params.get(property) is not None:
                     col_name = [col for col in scraped_data.columns if property.lower() in col.replace(" ","").lower()][0]
                     cur_data = cur_data[scraped_data[col_name] == str(req.query_params.get(property))]
                     print("Foud Matching Column Name: ",col_name)
-
     data = cur_data.to_json(orient='index')
     return json.loads(data)
 
