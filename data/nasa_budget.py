3257d1d0fdc200f7d053c2668e060037ca023f80
div <div.pat499@gmail.com>
2022-04-13 20:20:51 -0400

    data cleanup for Mission Costs summary only

--- data/nasa_budget.py
+++ data/nasa_budget.py
@@ -1,13 +1,14 @@
 import os
 from dotenv import load_dotenv
 import requests, json
+import pandas as pd
 load_dotenv()
 
 SHEETS_API = os.getenv('SHEETS_API_KEY')
 SPREADSHEET_ID = "12frTU01gfT1CXGWFimN3whf4348F_r3XolTqBt02OyM"
 
 
-def get_link(sheet_name):
+def get_data_json(sheet_name):
     ADD_KEY = f"?key={SHEETS_API}"
     NASA_MISSION_BUDGET = f"https://sheets.googleapis.com/v4/spreadsheets/{SPREADSHEET_ID}/values/"
 
@@ -17,8 +18,24 @@ def get_link(sheet_name):
         url = requests.get(URL).text
     except:
         return("Unhandled Exception")
-    return url
+    
+    # we only need the values from the google sheets API 
+    data_json = json.loads(url)["values"]
+
+    df_sheet = pd.DataFrame.from_dict(data_json)
+
+    return df_sheet
     
 
-data = json.loads(get_link("Mission Costs"))
-print(data)
+
+
+def get_mission_costs(only_summary = False):
+    df_sheet = get_data_json("Mission Costs")
+    # drop all rows except the first 4
+    df_sheet = df_sheet.drop( df_sheet.index.to_list()[4:] ,axis = 0 )
+    df_sheet = df_sheet.transpose()
+    print(df_sheet)
+    # if its only summary, we only need the first 
+    
+get_mission_costs()
+# print(json_data)
