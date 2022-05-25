3a387384f93be6cc0785e22a0f32ef4c3e62197f
div <div.pat499@gmail.com>
2022-05-24 22:58:20 -0400

    Improved scraping to add only relevant links

--- data/pds_parser.py
+++ data/pds_parser.py
@@ -5,6 +5,8 @@ PDS_WEBSTIE_BASE = "https://pds-rings.seti.org"
 PDS_GALLERY_BASE = "/galleries"
 HTML_EXT = ".html"
 
+IGNORE_LINK_NAMES = {"<< first", "< previous"}
+IGNORE_LINK_HREF = {"/galleries.html"}
 website = requests.get(PDS_WEBSTIE_BASE + PDS_GALLERY_BASE + HTML_EXT)
 
 soup = BeautifulSoup(website.content, 'html.parser')
@@ -19,3 +21,20 @@ for link in links:
     if href and "galleries/" in href:
         target_links.append(PDS_WEBSTIE_BASE + href)
 
+
+links_to_scrape = []
+try:
+    for link in target_links:
+        website = requests.get(link)
+
+        soup = BeautifulSoup(website.content, 'html.parser')
+        links = soup.find_all('a')
+        for l in links:
+                if l.string and l.string not in IGNORE_LINK_NAMES:
+                    if "html" in l.get('href') and l.get('href') not in IGNORE_LINK_HREF:
+                        links_to_scrape.append(l.get('href'))
+except:
+    print(f"error scraping link {link}")
+
+for l in links_to_scrape:
+    print(l)
