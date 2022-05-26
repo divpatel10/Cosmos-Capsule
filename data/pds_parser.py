084bd3644c9f62dd24fce098ae7ec1e5bd7e00e5
div <div.pat499@gmail.com>
2022-05-25 21:23:51 -0400

    Updated the scraping to effectively scrape for target links in one single loop

--- data/pds_parser.py
+++ data/pds_parser.py
@@ -1,40 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
 
-PDS_WEBSTIE_BASE = "https://pds-rings.seti.org"
-PDS_GALLERY_BASE = "/galleries"
+PDS_WEBSITE_BASE = "https://pds-rings.seti.org/"
+PDS_WEBSTIE_ALL_START = "https://pds-rings.seti.org/galleries/all_1986.html"
+
+PDS_PRESS_RELEASES = "/press_releases/pages/"
 HTML_EXT = ".html"
+NEXT_PAGE_NAME = "next >"
 
-IGNORE_LINK_NAMES = {"<< first", "< previous"}
-IGNORE_LINK_HREF = {"/galleries.html"}
-website = requests.get(PDS_WEBSTIE_BASE + PDS_GALLERY_BASE + HTML_EXT)
+to_scrape_links = [PDS_WEBSTIE_ALL_START]
 
-soup = BeautifulSoup(website.content, 'html.parser')
+while to_scrape_links:
+    to_scrape_link = to_scrape_links.pop()
+    
+    website = requests.get(to_scrape_link)    
+    soup = BeautifulSoup(website.content, 'html.parser')
 
-# Grab all links from the base page
-links = soup.find_all('a')
+    # Grab all links from the page
+    links = soup.find_all('a')
 
-# Store all links to be scraped for images into a list
-target_links = []
-for link in links:
-    href = link.get('href')
-    if href and "galleries/" in href:
-        target_links.append(PDS_WEBSTIE_BASE + href)
+    # Store all links to be scraped for images into a list
+    target_links = []
 
-
-links_to_scrape = []
-try:
-    for link in target_links:
-        website = requests.get(link)
-
-        soup = BeautifulSoup(website.content, 'html.parser')
-        links = soup.find_all('a')
-        for l in links:
-                if l.string and l.string not in IGNORE_LINK_NAMES:
-                    if "html" in l.get('href') and l.get('href') not in IGNORE_LINK_HREF:
-                        links_to_scrape.append(l.get('href'))
-except:
-    print(f"error scraping link {link}")
-
-for l in links_to_scrape:
-    print(l)
+    for link in links:
+        website = link.get('href')
+        
+        if website:
+            # add all press release pages to the target list
+            if HTML_EXT in website and PDS_PRESS_RELEASES in website:
+                target_links.append(website)
+            # add all next pages to the scraping list  
+            elif link.string == "next >":
+                to_scrape_link.append(website)
+                # print(website)
